316889992ced84c3f7257859b01fa2051726d664
evgarthub <evgart.kiev@gmail.com>
2022-11-06 13:39:43 +0200

    basic structure

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBw9PBbMuRYi3kgTXe-i4IUp17N4h4K6Eo",
+  authDomain: "courtbook-dev.firebaseapp.com",
+  projectId: "courtbook-dev",
+  storageBucket: "courtbook-dev.appspot.com",
+  messagingSenderId: "969956292377",
+  appId: "1:969956292377:web:fd86951e850f06891f3788",
+  measurementId: "G-Y46PDNZH3R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
